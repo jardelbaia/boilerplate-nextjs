e4552a568569e869822f19a15cc06eff0d7b5e82
Jardel Baia <jardelbaiajr@gmail.com>
2022-10-19 18:54:18 -0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+const isProd = (process.env.NODE_ENV = 'production')
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
