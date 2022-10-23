a00acfe0e15599f8873b55e74a3a0910f6a01c4e
Jardel Baia <jardelbaiajr@gmail.com>
2022-10-23 12:14:38 -0300

    temporarily remove pwa support

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
 
-const isProd = (process.env.NODE_ENV = 'production')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true
 }
 
-module.exports = withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
+module.exports = nextConfig
